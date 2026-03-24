c49a159e3ea72dfdf25dcb71e99e1bc07d8c7863
Overforge <190232855+TheOverforge@users.noreply.github.com>
2026-03-24 19:31:44 +0300

    Refactor binary search to class-based implementation
    Refactor binary search into a class method and update parameters.

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -1,22 +1,18 @@
-def binary_search(list, item):
-    low = 0
-    high = len(list)
+class Solution:
+    def search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
 
-    while low <= high:
-        mid = (low + high) // 2
-        guess = list[mid]
+        while low <= high:
+            mid = (low + high) // 2
+            guess = nums[mid]
 
-        if guess == item:
-            return mid
-        
-        if guess >= item:
-            high = mid - 1
+            if guess == target:
+                return mid
 
-        else:
-            low = mid + 1
+            if guess > target:
+                high = mid - 1
+            else:
+                low = mid + 1
 
-    return None
-my_list = [1,3,5,7,9]
-
-print (binary_search(my_list, 3))
-print (binary_search(my_list, -1))
+        return -1
